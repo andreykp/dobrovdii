b2263f536f7ff8747b9d5184efc31d5f7c0da8bb
Andrey K <ak@kitsoft.kiev.ua>
2022-05-06 14:46:28 +0300

    fix slider

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ window.onload = function() {
   /* SLIDER enable */
   $('.events-list').slick({
       infinite: true,
-      slidesToShow: 3
+      slidesToShow: 3,
+      prevArrow: '<button class="slick-prev" aria-label="Previous" type="button">&lt;</button>',
+      nextArrow: '<button class="slick-next" aria-label="Next" type="button">&gt;</button>',
+
   });
 
 }
